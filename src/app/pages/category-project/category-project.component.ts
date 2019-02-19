ee1a04b566fdef5131af9228a41f921ea64376c3
Paul Calabrese <pacalabre@gmail.com>
2019-02-18 19:00:21 -0800

    added filtering by slug category

--- src/app/pages/category-project/category-project.component.ts
+++ src/app/pages/category-project/category-project.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { WordpressService } from '../../wordpress-service.service';
 
 @Component({
@@ -6,14 +7,37 @@ import { WordpressService } from '../../wordpress-service.service';
   templateUrl: './category-project.component.html',
   styleUrls: ['./category-project.component.css']
 })
-export class CategoryProjectComponent {
+export class CategoryProjectComponent implements OnInit {
+  slugName="";
   posts = [];
+  categoryPosts = [];
 
-  constructor(private wp: WordpressService) { }
+  constructor(private route: ActivatedRoute,private wp: WordpressService) { }
+
+  ngOnInit() {
+    this.route.paramMap
+    .subscribe(paramsAsMap => {
+      this.slugName = this.route.snapshot.paramMap.get("category");
+      console.log("slug = "+ this.slugName);
+    })
+    this.filterPostsByCategory();
+  }
 
   ngDoCheck() {
     this.posts = this.wp.getPostsFromService();
-    console.log(this.posts);
+    this.filterPostsByCategory();
+  }
+
+  filterPostsByCategory() {
+    if(this.categoryPosts.length === 0) {
+      this.posts.forEach(post => {
+        let categoryName = post['_embedded']['wp:term'][0][0].slug;
+        if(categoryName === this.slugName) {
+          this.categoryPosts.push(post);
+        };
+      })
+    }
+      console.log(this.categoryPosts);
   }
 
 }
